921e44d27e98f77f86d2e13cb946e29030eea667
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2025-10-30 14:41:52 +0200

    Completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
-	}
+		double currentValue =Double.parseDouble(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		double years = Double.parseDouble(args[2]);
+		for (int i = 0; i < years; i++) {
+			currentValue = currentValue * (1 + rate/100);
+		}
+		System.out.println((int) currentValue);
+	}	
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -13,5 +13,20 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         // Replace this comment with the rest of your code
+		String ampm = "";
+		String timeInAmpm = hours + ":";
+		if (hours < 12){
+			ampm = "AM";
+		} else{
+			ampm = "PM";
+		}
+		if (minutes<10) {
+			String newMinutes = "0" + minutes;
+			timeInAmpm = timeInAmpm + newMinutes + " " +ampm;
+		} else{
+			timeInAmpm = timeInAmpm + minutes + " " + ampm;
+
+		}
+		System.out.println(timeInAmpm);
 	}
 }
